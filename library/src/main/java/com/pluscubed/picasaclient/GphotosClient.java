a2e01a5eddf2834fd942dc2ed3a4cdbfbe4da32e
Yossi RIzgan <yosriz@gmail.com>
2016-11-17 23:11:21 +0200

    fix NPE, add support for pagination, and getting AlbumEntry only

--- library/src/main/java/com/pluscubed/picasaclient/GphotosClient.java
+++ library/src/main/java/com/pluscubed/picasaclient/GphotosClient.java
@@ -9,20 +9,40 @@ import rx.functions.Func1;
 
 public class GphotosClient {
 
-    public Single<AlbumFeed> getGphotosAlbumFeed() {
+    public Single<AlbumEntry> getGphotosAlbum() {
         return PicasaClient.get().getUserFeed()
-                .flatMap(new Func1<UserFeed, Single<? extends AlbumFeed>>() {
+                .map(new Func1<UserFeed, AlbumEntry>() {
                     @Override
-                    public Single<? extends AlbumFeed> call(UserFeed userFeed) {
-                        long id = -1;
+                    public AlbumEntry call(UserFeed userFeed) {
+                        AlbumEntry  album = null;
                         for (AlbumEntry entry : userFeed.getAlbumEntries()) {
-                            if (entry.getGphotoAlbumType().equals(AlbumEntry.TYPE_GOOGLE_PHOTOS)) {
-                                id = entry.getGphotoId();
+                            if (AlbumEntry.TYPE_GOOGLE_PHOTOS.equals(entry.getGphotoAlbumType())) {
+                                album = entry;
+                                break;
                             }
                         }
-
-                        return PicasaClient.get().getAlbumFeed(id);
+                        return album;
                     }
                 });
+    }
+
+    public Single<AlbumFeed> getGphotosAlbumFeed() {
+        return getGphotosAlbum().flatMap(new Func1<AlbumEntry, Single<AlbumFeed>>() {
+            @Override
+            public Single<AlbumFeed> call(AlbumEntry albumEntry) {
+                return PicasaClient.get()
+                        .getAlbumFeed(albumEntry.getGphotoId());
+            }
+        });
+    }
+
+    public Single<AlbumFeed> getGphotosAlbumFeed(final int startIndex, final int maxResults) {
+        return getGphotosAlbum().flatMap(new Func1<AlbumEntry, Single<AlbumFeed>>() {
+            @Override
+            public Single<AlbumFeed> call(AlbumEntry albumEntry) {
+                return PicasaClient.get()
+                        .getAlbumFeed(albumEntry.getGphotoId(), startIndex, maxResults);
+            }
+        });
     }
 }
