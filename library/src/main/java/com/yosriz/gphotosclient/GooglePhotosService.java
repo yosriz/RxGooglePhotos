30a34fc5abd82e677875b9486fc9bf42221b28cc
Yossi RIzgan <yosriz@gmail.com>
2017-06-18 21:16:58 +0300

    add some documentation

--- library/src/main/java/com/yosriz/gphotosclient/GooglePhotosService.java
+++ library/src/main/java/com/yosriz/gphotosclient/GooglePhotosService.java
@@ -1,7 +1,6 @@
 package com.yosriz.gphotosclient;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
-
 import com.yosriz.gphotosclient.model.AlbumEntry;
 import com.yosriz.gphotosclient.model.AlbumFeed;
 import com.yosriz.gphotosclient.model.AlbumFeedResponse;
@@ -23,12 +22,25 @@ public class GooglePhotosService {
         this.account = account;
     }
 
+    /**
+     * Get user feed, feed contains all user various albums and photos collection.
+     * <p>there's a limit of 1,000 albums per response, for getting paged result use {@link #getUserFeed(int, int)}.</p>
+     *
+     * @return {@link UserFeed} object.
+     */
     public Single<UserFeed> getUserFeed() {
         return picasaApi.getUserFeedResponse()
                 .map(UserFeedResponse::getFeed)
                 .subscribeOn(Schedulers.io());
     }
 
+    /**
+     * Get user feed, feed contains all user various albums and photos collection.
+     *
+     * @param startIndex start album index (first index is 1)
+     * @param maxResults max results
+     * @return {@link UserFeed} object.
+     */
     public Single<UserFeed> getUserFeed(int startIndex, int maxResults) {
         if (startIndex < 1) {
             throw new IllegalArgumentException("Illegal start index, must be above 0");
@@ -38,6 +50,13 @@ public class GooglePhotosService {
                 .subscribeOn(Schedulers.io());
     }
 
+    /**
+     * Get album feed by id. {@link AlbumFeed} photos can be retrieved using {@link AlbumFeed#getPhotoEntries()}.
+     * <p>there's a limit of 1,000 photos per response, for getting paged result use {@link #getAlbumFeed(long, int, int)}.</p>
+     *
+     * @param albumId album id
+     * @return {@link AlbumFeed}
+     */
     public Single<AlbumFeed> getAlbumFeed(long albumId) {
         return picasaApi.getAlbumFeedResponse(albumId)
                 .map(AlbumFeedResponse::getFeed)
@@ -45,6 +64,14 @@ public class GooglePhotosService {
 
     }
 
+    /**
+     * Get album feed by id. {@link AlbumFeed} photos can be retrieved using {@link AlbumFeed#getPhotoEntries()}.
+     *
+     * @param albumId    album id
+     * @param startIndex start photo index (first index is 1)
+     * @param maxResults max results
+     * @return {@link AlbumFeed}
+     */
     public Single<AlbumFeed> getAlbumFeed(long albumId, int startIndex, int maxResults) {
         if (startIndex < 1) {
             throw new IllegalArgumentException("Illegal start index, must be above 0");
@@ -54,6 +81,11 @@ public class GooglePhotosService {
                 .subscribeOn(Schedulers.io());
     }
 
+    /**
+     * Get Instant upload album, this album, contains all photos that are backed up automatically by Google Photos.
+     *
+     * @return {@link AlbumEntry} represents instant upload album
+     */
     public Single<AlbumEntry> getGooglePhotosInstantUploadAlbum() {
         return getUserFeed()
                 .flattenAsObservable(UserFeed::getAlbumEntries)
